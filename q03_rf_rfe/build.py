17bab74892395d3871906b66ece069da0837d640
sharax <sharathdharmaji@gmail.com>
2017-11-02 04:06:30 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -9,4 +10,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Your solution code here
 
+def rf_rfe(data):
+    X=data.iloc[:,:-1]
+    y=data.iloc[:,-1]
+    model=RandomForestClassifier()
+    rfe=RFE(model)
+    rfe=rfe.fit(X,y)
+    c=rfe.ranking_
+    d=[]
+    e=[]
+    for i in range(0,len(c)):
+        if c[i]==1:
+            d.append(i)
+    for j in d:
+        e.append(list(X)[j])
+    return e
 
+#model=RandomForest
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -6,5 +7,22 @@ import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+np.random.seed(9)
+# Yourr solution code here
+def select_from_model(data):
+    X=data.iloc[:,:-1]
+    y=data.iloc[:,-1]
+    model=RandomForestClassifier()
+    c=model.fit(X,y)
+    model1=SelectFromModel(c ,prefit=True)#embedded
+    d=model1.get_support()
+    e=[]
+    for i in range(0,len(d)):
+        if d[i]==True:
+            e.append(i)
+    f=[]
+    for i in e:
+        f.append(list(X)[i])
+    return f
 
-# Your solution code here
+#select_from_model(data)
